36f9c5fb95eff35ace36d224137915a60639f481
Sang Un Lee <sangunlee6@gmail.com>
2024-03-12 01:07:03 +0900

    Feat: memo CRUD 구현

--- src/memo/memo.controller.ts
+++ src/memo/memo.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { MemoService } from './memo.service';
+import { MemoEntity } from './memo.entity';
 
 @Controller('memo')
 export class MemoController {
   constructor(private readonly memoService: MemoService) {}
+
+  @Get('/')
+  async findAll(): Promise<MemoEntity[]> {
+    return this.memoService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<MemoEntity> {
+    return this.memoService.findOne(id);
+  }
+
+  @Post('/')
+  async create(@Body() memo: MemoEntity): Promise<MemoEntity> {
+    return this.memoService.create(memo);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() memo: MemoEntity,
+  ): Promise<void> {
+    await this.memoService.update(id, memo);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    await this.memoService.delete(id);
+  }
 }
--- src/memo/memo.service.ts
+++ src/memo/memo.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MemoEntity } from './memo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MemoService {}
+export class MemoService {
+  constructor(
+    @InjectRepository(MemoEntity)
+    private memoRepository: Repository<MemoEntity>,
+  ) {}
+
+  async findAll(): Promise<MemoEntity[]> {
+    return await this.memoRepository.find();
+  }
+
+  private async findMemoById(id: number): Promise<MemoEntity> {
+    const foundMemo = await this.memoRepository.findOne({
+      where: { id },
+    });
+
+    if (!foundMemo) {
+      throw new NotFoundException(`Memo with ID ${id} not found`);
+    }
+
+    return foundMemo;
+  }
+
+  async findOne(id: number): Promise<MemoEntity> {
+    return await this.findMemoById(id);
+  }
+
+  async create(memo: MemoEntity): Promise<MemoEntity> {
+    return await this.memoRepository.save(memo);
+  }
+
+  async update(id: number, memo: MemoEntity): Promise<void> {
+    await this.findMemoById(id);
+    await this.memoRepository.update(id, memo);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.findMemoById(id);
+    await this.memoRepository.delete(id);
+  }
+}
