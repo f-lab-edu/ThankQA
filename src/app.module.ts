fcc472fef84da0fb0c47a4655132c30f292690f4
Sang Un Lee <sangunlee6@gmail.com>
2024-03-17 15:32:59 +0900

    Chore: file 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MemoModule } from './memo/memo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'config/TypeORM.config';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), MemoModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MemoModule, FileModule],
   controllers: [AppController],
   providers: [AppService],
 })
