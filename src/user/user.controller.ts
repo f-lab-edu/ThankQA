ef76abdec4dd020acb20e3473bb653a9b83d4693
Sang Un Lee <sangunlee6@gmail.com>
2024-03-24 01:16:08 +0900

    Feat: User CRUD API 생성#7

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete } from '@nestjs/common';
+import User from './user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    const users = await this.userService.getAllUsers();
+    return users;
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    const user = await this.userService.getUserById(Number(id));
+    return user;
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const newUser = await this.userService.createUser(createUserDto);
+    return newUser;
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string): Promise<User> {
+    const user = this.userService.deleteById(Number(id));
+    return user;
+  }
 }
