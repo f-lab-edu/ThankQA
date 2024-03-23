5316d9024f1a21731f9af81e1b9c2d6b6e72e621
Sang Un Lee <sangunlee6@gmail.com>
2024-03-24 01:07:42 +0900

    Feat: User 엔티티 생성#7

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'config/TypeORM.config';
 import { FileModule } from './file/file.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     MemoModule,
     FileModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
